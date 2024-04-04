d7f928df576178c0cde0ac678fa0932220e2f57c
Alex Hery <alex.herascu@hery.co.uk>
2024-04-04 13:42:22 +0100

    ready to send

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -26,131 +26,133 @@ class _LandingPageState extends State<LandingPage> {
         padding:const EdgeInsets.all(10),
         width: SizeConfig.screenWidth,
         height: SizeConfig.screenHeight,
-        child: Column(
-          children: [
-            FittedBox(
-              fit: BoxFit.contain,
-              child: SizedBox(
-                width: SizeConfig.screenWidth,
-                height: SizeConfig.safeBlockVertical! * 10,
-                child: Image.asset(
-                  'assets/images/ordreGroup.jpeg',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: SizeConfig.blockSizeVertical! * 10,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                elevation: MaterialStateProperty.all<double>(5.0),
-                shape: MaterialStateProperty.all<OutlinedBorder>(
-                  const RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.black87),
-                    borderRadius:
-                        BorderRadius.all(Radius.circular(8)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              FittedBox(
+                fit: BoxFit.contain,
+                child: SizedBox(
+                  width: SizeConfig.screenWidth,
+                  height: SizeConfig.safeBlockVertical! * 10,
+                  child: Image.asset(
+                    'assets/images/ordreGroup.jpeg',
                   ),
                 ),
-                shadowColor: MaterialStateProperty.all<Color>(
-                    Colors.black54,
-                ),
-                backgroundColor: MaterialStateProperty.all<Color>(
-                   Colors.blue[100]!,
-                ),
               ),
-              onPressed: () async{
-
-                await availableCameras().then(
-                (value) {
-                   Navigator.of(context).pushNamed('/camera',arguments: value);
-                }
-              );
-               
-              },
-              child: Container(
-                padding: const  EdgeInsets.all(20),
-                child:const Column(
-                  children: [
-                     SizedBox(
-                      height: 10,
-                    ),
-                    Icon(
-                      Icons.camera,
-                      size: 40,
-                      color: Colors.black54,
-                    ),
-                     SizedBox(
-                      height: 10,
-                    ),
-                     Text.rich(
-                        textAlign:  TextAlign.center ,
-                        TextSpan(
-                          text: 'Take a picture',
-                          style: TextStyle(
-                            color:  Colors.black87,
-                            fontSize:  25 ,
-                            fontWeight: FontWeight.w500
-                          )
-                        ),
-                      ),
-                  ],
-                ),
-              ),),
               SizedBox(
-                height: SizeConfig.safeBlockVertical! * 5,
+                height: SizeConfig.blockSizeVertical! * 10,
               ),
-              Container(
-                width: 250,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2,
-                    color: Colors.black87,
+              ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all<double>(5.0),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    const RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black87),
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(8)),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(10)
+                  shadowColor: MaterialStateProperty.all<Color>(
+                      Colors.black54,
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                     Colors.blue[100]!,
+                  ),
                 ),
-                child: StreamBuilder(
-                    stream: PictureProvider.of(context)!.pictureStream.imageStream,
-                    builder: (context, image) { 
-                       if(!image.hasData){
-                        return const Center(
-                          child:  Text.rich( TextSpan(
-                            text: 'No picture has been taken.',
+                onPressed: () async{
+          
+                  await availableCameras().then(
+                  (value) {
+                     Navigator.of(context).pushNamed('/camera',arguments: value);
+                  }
+                );
+                 
+                },
+                child: Container(
+                  padding: const  EdgeInsets.all(20),
+                  child:const Column(
+                    children: [
+                       SizedBox(
+                        height: 10,
+                      ),
+                      Icon(
+                        Icons.camera,
+                        size: 40,
+                        color: Colors.black54,
+                      ),
+                       SizedBox(
+                        height: 10,
+                      ),
+                       Text.rich(
+                          textAlign:  TextAlign.center ,
+                          TextSpan(
+                            text: 'Take a picture',
                             style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
+                              color:  Colors.black87,
+                              fontSize:  25 ,
+                              fontWeight: FontWeight.w500
                             )
-                          )),
-                        );
-                       }
-                       return  FutureBuilder(
-                         future: getGrayscale(image.data!),
-                         builder: (context, grayscaleImage) {
-                          if(!grayscaleImage.hasData){
-                            return const Center(
-                              child:  Text.rich( TextSpan(
-                                text: 'Transforming ...',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                )
-                              )),
-                            );
-                          }
-
-
-
-                           return Column(mainAxisSize: MainAxisSize.min, children: [
-                            const SizedBox(height: 20),
-                            Image.memory(grayscaleImage.data!, fit: BoxFit.cover, width: 200),
-                            const SizedBox(height: 20),
-                          ]);
-                         }
-                       );
-                    }
+                          ),
+                        ),
+                    ],
                   ),
-              ),
-              
-          ],
+                ),),
+                SizedBox(
+                  height: SizeConfig.safeBlockVertical! * 5,
+                ),
+                Container(
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.black87,
+                    ),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: StreamBuilder(
+                      stream: PictureProvider.of(context)!.pictureStream.imageStream,
+                      builder: (context, image) { 
+                         if(!image.hasData){
+                          return const Center(
+                            child:  Text.rich( TextSpan(
+                              text: 'No picture has been taken.',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              )
+                            )),
+                          );
+                         }
+                         return  FutureBuilder(
+                           future: getGrayscale(image.data!),
+                           builder: (context, grayscaleImage) {
+                            if(!grayscaleImage.hasData){
+                              return const Center(
+                                child:  Text.rich( TextSpan(
+                                  text: 'Transforming ...',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                  )
+                                )),
+                              );
+                            }
+          
+          
+          
+                             return Column(mainAxisSize: MainAxisSize.min, children: [
+                              const SizedBox(height: 20),
+                              Image.memory(grayscaleImage.data!, fit: BoxFit.cover, width: 200),
+                              const SizedBox(height: 20),
+                            ]);
+                           }
+                         );
+                      }
+                    ),
+                ),
+                
+            ],
+          ),
         ),
       ),
     );
